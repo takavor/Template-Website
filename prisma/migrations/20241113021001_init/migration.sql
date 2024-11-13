084fd2df859c73f4e3155c0f70d2b12cf27a5569
Sevag <baghsev@gmail.com>
2024-11-12 21:24:07 -0500

    Update navbar to use server session

--- prisma/migrations/20241113021001_init/migration.sql
+++ prisma/migrations/20241113021001_init/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "User" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "email" TEXT NOT NULL,
-    "name" TEXT NOT NULL
+    "name" TEXT
 );
 
 -- CreateIndex
--- prisma/migrations/20241113021213_init/migration.sql
+++ prisma/migrations/20241113021213_init/migration.sql
@@ -2,8 +2,20 @@
   Warnings:
 
   - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
 
 */
+-- CreateTable
+CREATE TABLE "Organization" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "productsDescription" TEXT,
+    "keyWords" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Organization_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
 -- RedefineTables
 PRAGMA defer_foreign_keys=ON;
 PRAGMA foreign_keys=OFF;
